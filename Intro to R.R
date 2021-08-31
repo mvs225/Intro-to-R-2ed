e02dbe671b7dc6e3de720a066876f3a98c5cf61f
Meaghan stack <mvs225@lehigh.edu>
2021-08-31 18:00:45 -0400

    took out #

--- Intro to R.R
+++ Intro to R.R
@@ -1,9 +1,9 @@
 # R script to accompany Intro to R
 # Copy for mvs225
-######################################################
+##################################################
 
 # You will see that you can put non-executable "comment" lines
 # in your script by putting "#" at the beginning of the lines.
-# 
+# customized by mvs225
 # In fact, you can put comments anywhere on a line as long as 
 # you put "#" before them.
