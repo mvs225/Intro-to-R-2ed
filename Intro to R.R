906b951f1ecb77e6c73ea931412621a53d108d24
Meaghan stack <mvs225@lehigh.edu>
2021-08-31 18:13:42 -0400

    added name

--- Intro to R.R
+++ Intro to R.R
@@ -1,5 +1,5 @@
 # R script to accompany Intro to R
-# Copy for mvs225
+# Copy for student mvs225
 ##################################################
 
 # You will see that you can put non-executable "comment" lines
